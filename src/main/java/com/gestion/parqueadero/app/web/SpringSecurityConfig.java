5835c734af976ec8b5c872e35dae4cd98f0d9d38
Gaby <herreragaby191996@gmail.com>
2019-12-06 07:53:38 -0500

    security

---
+++ src/main/java/com/gestion/parqueadero/app/web/SpringSecurityConfig.java
@@ -0,0 +1,65 @@
+package com.gestion.parqueadero.app.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.gestion.parqueadero.app.web.security.LoginSuccessHandler;
+
+@EnableGlobalMethodSecurity(securedEnabled=true)
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private LoginSuccessHandler successHandler;
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(); 		
+	}
+
+	@Autowired
+	public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception {		
+		PasswordEncoder encoder = passwordEncoder();
+		UserBuilder users = 
+				User.builder().passwordEncoder(password -> encoder.encode(password));
+		builder.inMemoryAuthentication()
+			.withUser(users.username("admin1").password("12345").roles("ADMIN"))			
+			.withUser(users.username("diego").password("12345").roles("USER"));		
+	}
+
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/","/css/**","/js/**","/images/**").permitAll()
+			.antMatchers("/estacionamiento/**").hasAnyRole("ADMIN")			
+			.antMatchers("/vehiculo/**").hasAnyRole("ADMIN")
+			.antMatchers("/propietario/**").hasAnyRole("ADMIN")
+			.antMatchers("/area/**").hasAnyRole("ADMIN")
+			.anyRequest().authenticated()
+			.and()
+				.formLogin().successHandler(successHandler)
+				.loginPage("/login").permitAll()
+			.and()
+				.logout().permitAll()
+			.and()
+				.exceptionHandling().accessDeniedPage("/error_403")
+			.and()
+				.csrf().ignoringAntMatchers("/h2-console/**")
+			.and()
+				.headers().frameOptions().sameOrigin();
+
+
+	}
+
+
+}
--- src/main/java/com/gestion/parqueadero/app/web/controllers/HomeController.java
+++ src/main/java/com/gestion/parqueadero/app/web/controllers/HomeController.java
@@ -1,9 +1,12 @@
 package com.gestion.parqueadero.app.web.controllers;
+import java.security.Principal;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping(value="/")
@@ -11,9 +14,23 @@ public class HomeController {
 	
 	@GetMapping(value="/")
 	public String home(Model model) {
-		model.addAttribute("tittle", "SGPE");
-		model.addAttribute("description", "Sistema de Gestion de Parqueaderos");
+		model.addAttribute("title", "SGPE");
+		model.addAttribute("description", "Sistema de Gestion de Parqueaderos. El sistema de gestión de Parqueaderos administra entradas y salidas de vehículos al parqueadero, mantiene un inventario de las áreas de estacionamiento, hace seguimiento a los horarios en que debería utilizarse el servicio por todos los usuarios, registra las incidencias de uso inadecuado del recurso brindado y muestra el valor a pagar por el estacionamiento. ");
 		return "home";
+	}
+	
+	@GetMapping(value="/login")
+	public String login(@RequestParam(value="error", required=false) String error, 
+			Model model, Principal principal, RedirectAttributes flash) {
+
+		if(principal != null) {
+			flash.addFlashAttribute("info", "El usuario ya tiene una sesión activa.");
+			return "redirect:/";
+		}		
+		if(error != null) {
+			model.addAttribute("error", "Usuario o contraseña incorrectas");
+		}				
+		return "login";
 	}
 
 }
